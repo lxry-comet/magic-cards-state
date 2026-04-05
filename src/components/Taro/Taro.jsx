099eaa9fb05b8cec5b292a5b3522d08ac0e87ab3
lxry-comet <durdentagirowich@gmail.com>
2026-04-05 22:13:44 +0200

    update_images

--- src/components/Taro/Taro.jsx
+++ src/components/Taro/Taro.jsx
@@ -1,40 +1,49 @@
-import {CardContainer,
-ItemTitle,
-ItemImage,
-ItemInter,
-ItemButton} from './Taro.styled.jsx';
-
+import {
+	CardContainer,
+	ItemButton,
+	ItemImage,
+	ItemInter,
+	ItemTitle
+} from './Taro.styled.jsx'
 
 export default function Taro({
-  name,
-  id,
-  suite,
-  description,
-  interpretation,
-  imageUrl 
+	name,
+	id,
+	suite,
+	description,
+	interpretation,
+	imageUrl
 }) {
-  return (
-    <CardContainer>
-      <ItemTitle>{name} (№{id})</ItemTitle>
-      {imageUrl && <ItemImage
-       src={imageUrl} alt={name} />} 
-      <>
-        <ItemInter><strong>Інтерпретація:</strong> {interpretation}</ItemInter>
-      </>
-    
-      <br />
-      <ItemButton
-  onMouseEnter={(e) => {
-    e.target.style.backgroundColor = '#d4af37';
-    e.target.style.color = '#000';
-  }}
-  onMouseLeave={(e) => {
-    e.target.style.backgroundColor = 'transparent';
-    e.target.style.color = '#d4af37';
-  }}
->
-  Обрати карту
-</ItemButton>
-    </CardContainer> 
-  );
+	return (
+		<CardContainer>
+			<ItemTitle>
+				{name} (№{id})
+			</ItemTitle>
+			{imageUrl && (
+				<ItemImage
+					src={import.meta.env.BASE_URL + imageUrl.slice(1)}
+					alt={name}
+				/>
+			)}
+			<>
+				<ItemInter>
+					<strong>Інтерпретація:</strong> {interpretation}
+				</ItemInter>
+			</>
+
+			<br />
+			<ItemButton
+				onMouseEnter={e => {
+					e.target.style.backgroundColor = '#d4af37'
+					e.target.style.color = '#000'
+				}}
+				onMouseLeave={e => {
+					e.target.style.backgroundColor = 'transparent'
+					e.target.style.color = '#d4af37'
+				}}
+			>
+				Обрати карту
+			</ItemButton>
+		</CardContainer>
+	)
 }
